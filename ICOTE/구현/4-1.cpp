4ac0a267edbc88aaddc63005e8f53683e2afa2fb
kryptonite43 <sonyj0430@gmail.com>
2023-06-26 23:42:01 +0900

    4-1 상하좌우 (개선)

--- ICOTE/구현/4-1.cpp
+++ ICOTE/구현/4-1.cpp
@@ -6,10 +6,31 @@ int main() {
     int n, x = 1, y = 1, cnt = 0;
     string str;
     char c;
-    cin>>n;
+    int dx[4] = {0, 0, -1, 1};
+    int dy[4] = {-1, 1, 0, 0};
+    char moves[4] = {'L', 'R', 'U', 'D'};
+    cin >> n;
     cin.ignore();
     getline(cin, str);
-    for (int i=0; i<str.size(); i++) {
+
+    for (int i = 0; i < str.size(); i++) {
+        c = str[i];
+        for (int j = 0; j < 4; j++) {
+            if (c == moves[j]) {
+                int nx = x+dx[j];
+                int ny = y+dy[j];
+
+                if (nx==0||nx==n+1||ny==0|ny==n+1) continue;
+                else {
+                    x = nx;
+                    y = ny;
+                }
+                break;
+            }
+        }
+    }
+
+    /* for (int i=0; i<str.size(); i++) {
         c = str[i];
         switch(c) {
             case 'L':
@@ -32,7 +53,7 @@ int main() {
         else if (x == n + 1 | y == n + 1){
                 (x == n + 1) ? x-- : y--;
         }
-    }
+    } */
     printf("%d %d", x, y);
     printf("\n\n");
     return 0;
