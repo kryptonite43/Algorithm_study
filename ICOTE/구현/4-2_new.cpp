dbb84b011b7cf5a2cb4242c9cbdab890f9497d5e
kryptonite43 <sonyj0430@gmail.com>
2024-05-18 20:32:03 +0900

    4-2 시각

--- ICOTE/구현/4-2_new.cpp
+++ ICOTE/구현/4-2_new.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <cstdio>
+#include <string>
 using namespace std;
 
-int main() {
+int main()
+{
+    int n = 0, ans = 0, bulk = 0;
+    cin >> n;
 
+    for (int min = 0; min < 60; min++)
+    {
+        for (int sec = 0; sec < 60; sec++)
+        {
+            string s = to_string(min) + to_string(sec);
+            if (s.find("3") != string::npos)
+                bulk++;
+        }
+    }
 
-    printf("\n\n");
+    for (int i = 0; i <= n; i++)
+    {
+        if (i == 3 || i == 13 || i == 23)
+        {
+            ans += 3600;
+            continue;
+        }
+        ans += bulk;
+    }
+
+    printf("%d\n\n", ans);
     return 0;
 }
+
+// 스위치문에 들어가는 case에는 숫자만 가능함
