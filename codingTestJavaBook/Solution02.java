cd998b9a7475c875c465f16d8939e20ffc3b2e6e
kryptonite43 <sonyj0430@gmail.com>
2024-12-10 17:49:05 +0900

    02 배열 제어하기 (개선)

--- codingTestJavaBook/Solution02.java
+++ codingTestJavaBook/Solution02.java
@@ -3,17 +3,19 @@ import java.util.stream.Collectors;
 
 public class Solution02 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new int[]{2, 1, 1, 3, 2, 5, 4})));
+        System.out.println(Arrays.toString(solution(new int[]{4,2,2,1,3,4})));
+        System.out.println(Arrays.toString(solution(new int[]{2,1,1,3,2,5,4})));
     }
 
     private static int[] solution(int[] arr) {
-        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        /*List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
         Set<Integer> set = new HashSet<Integer>(list);
         List<Integer> newList = new ArrayList<Integer>(set);
         Integer[] tmp = newList.toArray(new Integer[0]);
-        Arrays.sort(tmp);
+        Arrays.sort(tmp);*/
+        Integer[] tmp = Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new);
         Arrays.sort(tmp, Collections.reverseOrder());
-        int[] ans = Arrays.stream(tmp).mapToInt(i->i).toArray();
+        int[] ans = Arrays.stream(tmp).mapToInt(i->i).toArray(); // mapToInt(Integer::intValue)
         return ans;
     }
 }
