c560644e6c249f99538d238281e61c050bf1bd7b
kryptonite43 <sonyj0430@gmail.com>
2024-12-17 17:44:52 +0900

    [코딩테스트 입문] 배열의 평균값

---
+++ Programmers/배열의_평균값.java
@@ -0,0 +1,15 @@
+/** @noinspection NonAsciiCharacters*/
+public class 배열의_평균값 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        System.out.println(solution(new int[]{89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99}));
+    }
+
+    public static double solution(int[] numbers) {
+        double answer = 0;
+        for (int x: numbers) {
+            answer += x;
+        }
+        return answer / numbers.length;
+    }
+}
