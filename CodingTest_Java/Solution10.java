d54efd49d8244fcb78d9aacccd5d4ef907d2eb6a
kryptonite43 <sonyj0430@gmail.com>
2024-12-24 16:36:26 +0900

    [월간 코드 챌린지 시즌2] 괄호 회전하기 (다른 풀이 추가)

--- CodingTest_Java/Solution10.java
+++ CodingTest_Java/Solution10.java
@@ -5,13 +5,7 @@ public class Solution10 {
     public static void main(String[] args) {
         System.out.println(solution("[)(]"));
     }
-/*
-s	        result
-"[](){}"	3
-"}]()[{"	2
-"[)(]"	    0
-"}}}"	    0
-*/
+
     private static int solution(String s) {
         int answer = 0;
         StringBuilder sb = new StringBuilder(s);
@@ -23,7 +17,36 @@ s	        result
 
             if (checkString(sb)) answer++;
         }
+        /* 다른 풀이
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
 
+        int n = s.length();
+        s += s; // 원본 문자열을 2번 이어 붙여 회전을 간편하게 구현 (like: sliding window)
+
+        int answer = 0;
+
+        A:
+        for (int i = 0; i < n; i++) {
+            ArrayDeque<Character> stack = new ArrayDeque<>();
+
+            for (int j = i; j < i + n; i++) { // 원본 문자열에 해당하는 부분만 체크
+                char c = s.charAt(j);
+
+                if (!map.containsKey(c)) {
+                    stack.push(c);
+                } else {
+                    if (stack.isEmpty() || !stack.pop().equals(map.get(c)))
+                        continue A;
+                }
+            }
+
+            if (stack.isEmpty())
+                answer++;
+        }
+        */
         return answer;
     }
     private static boolean checkString(StringBuilder curStr) {
