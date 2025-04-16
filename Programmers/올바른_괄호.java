f7ddde7568ce261c8e1b73f519bf862a1f3a6e76
Yujin Son <sonyj0430@gmail.com>
2025-04-16 18:07:54 +0900

    Create 올바른_괄호.java

---
+++ Programmers/올바른_괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                st.push(c);
+            }
+            else { // ')' 일 시
+                if (st.isEmpty())
+                    return false;
+                else
+                    st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
