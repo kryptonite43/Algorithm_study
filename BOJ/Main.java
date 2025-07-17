c3de5d65c8e32eb3afb9079aefb9ec37a107cb54
kryptonite43 <sonyj0430@gmail.com>
2025-07-17 21:37:18 +0900

    fix

--- BOJ/Main.java
+++ BOJ/Main.java
@@ -1,14 +1,7 @@
 /* BOJ Java 제출 형식 템플릿 */
 public class Main {
     public void solution() throws Exception {
-        /* 1. 입력을 위한 코드: Scanner 대신 BufferedReader 사용하기
-        * BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        * int n = Integer.parseInt(br.readLine());
-        * ...
-        * 2. 공백 기준 String 입력: str.split 대신 StringTokenizer 사용하기
-        * StringTokenizer st = new StringTokenizer(br.readLine());
-        * int s = Integer.parseInt(st.nextToken());
-        * 3. 출력: System.out.println 대신 BufferedWriter/StringBuilder 사용하기
+        /* * 3. 출력: System.out.println 대신 BufferedWriter/StringBuilder 사용하기
         * 1) BufferedWriter의 경우
         * BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         * bw.write(String.valueOf(data)); // data는 int형
