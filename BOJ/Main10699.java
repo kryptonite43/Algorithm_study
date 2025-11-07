737d7ef07fd1936204051e8ed287cdeb7d618954
kryptonite43 <sonyj0430@gmail.com>
2025-11-07 17:44:20 +0900

    10699 오늘 날짜

---
+++ BOJ/Main10699.java
@@ -0,0 +1,10 @@
+
+    public class Main10699 {
+    public void solution() throws Exception {
+        System.out.println("2025-11-07");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main10699().solution();
+    }
+}
