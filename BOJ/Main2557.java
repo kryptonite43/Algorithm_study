33804aec6f6e54ac5424f187b9076dc244a37a16
kryptonite43 <sonyj0430@gmail.com>
2025-11-26 20:56:25 +0900

    2557 Hello World

---
+++ BOJ/Main2557.java
@@ -0,0 +1,9 @@
+public class Main2557 {
+    public void solution() throws Exception {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main2557().solution();
+    }
+}
